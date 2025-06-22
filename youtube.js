4b95172b7949012793dd39f0fe8c76cd19f4999b
Aarush-Parashar <aarushparashar2004@gmail.com>
2025-06-22 10:05:41 +0530

    edited scopes

--- youtube.js
+++ youtube.js
@@ -2,7 +2,9 @@ import { google } from 'googleapis';
 
 export const SCOPES = [
   'https://www.googleapis.com/auth/youtube.upload',
-  'https://www.googleapis.com/auth/youtube.force-ssl'
+  'https://www.googleapis.com/auth/youtube.force-ssl',
+  'https://www.googleapis.com/auth/userinfo.email',
+  'https://www.googleapis.com/auth/userinfo.profile'
 ];
 
 export function getOAuthClient() {
